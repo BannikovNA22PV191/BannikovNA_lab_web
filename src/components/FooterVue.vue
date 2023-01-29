a96770505968afae8bb39ad83f745f1d0f14f761
BannikovNA22PV191 <20BNkov0025@gmail.com>
2023-01-29 17:10:58 +0300

    add new branch

---
+++ src/components/FooterVue.vue
@@ -0,0 +1,41 @@
+<template>
+<!--  mt-auto position-sticky-->
+  <footer class="footer bg-dark text-center text-white mt-3">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Google -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://www.chitai-gorod.ru" role="button">
+          <i class="fab fa-google">Google</i></a>
+
+        <!-- Instagram -->
+        <a class="btn btn-outline-light btn-floating m-1" href="" role="button">
+          <i class="fab fa-instagram">Inst</i></a>
+
+        <!-- HH -->
+        <a class="btn btn-outline-light btn-floating m-1" href="" role="button">
+          <i class="fab fa-linkedin-in">HH</i></a>
+
+        <!-- Github -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/BannikovNA22PV191" role="button">
+          <i class="fab fa-github"></i>Git</a>
+
+      </section>
+
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      Copyright: Nikita Bannikov <br> © 2022
+      <a class="text-white" href="https://vk.com/wargolem"><br>VK: Nikita Bannikov</a>
+    </div>
+  </footer>
+
+</template>
+
+<script>
+
+export default {}
+
+</script>
---
+++ src/components/HeaderVue.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="">BookHouse</a>
+        <div>
+          <ul class="navbar-nav ">
+            <li class="nav-item">
+              <a class="nav-link mx-2" aria-current="page" href="">Каталог</a>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{name: 'DeliveryPage'}">Доставка</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</template>
+
+
+
+<script>
+
+
+export default {}
+
+</script>
